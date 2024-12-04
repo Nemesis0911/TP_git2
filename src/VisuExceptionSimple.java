2fafab361a61e30c7e16c78f9e358e9cdacae17d
Etienne Gonella <etienne.gonella@gmail.com>
2024-12-04 11:44:33 +0100

    Semi fin java exception

--- src/VisuExceptionSimple.java
+++ src/VisuExceptionSimple.java
@@ -1,10 +1,16 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class VisuExceptionSimple{
     public void testexceptionscanner(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entrer un entier pour que saisie OK et autre chose pour exception");
-        sc.nextInt();
+        try {
+            System.out.println("Entrer un entier pour que saisie OK et autre chose pour exception");
+            sc.nextInt();
+        }
+        catch (InputMismatchException e){
+            System.out.print("Vous n'avez pas bien respecté les contraintes de type en entree !" +e.toString());
+        }
         System.out.print("fini !");
     }
 }
