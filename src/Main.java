5af7737adacb9e40c8edaca79ddff3b06701141c
Joshua SIX <joshua.six@etud.univ-jfc.fr>
2024-12-04 10:04:27 +0100

    coeur sur vous

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+package org.github.amandineLaymajoux;
+
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Main extends Canvas implements WindowListener {
     public static void main(String[] args) {
-        //coucou camcam
-        System.out.println("Normalement il y aura conflit");
-        }
+
+        Main main = new Main();
+        Frame f = new Frame();
+        f.add(main);
+        f.setTitle("Coucou les copains !!");
+        f.setSize(200, 150);
+        f.setVisible(true);
+        f.addWindowListener(main);
     }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        // Couleur du cœur
+        g.setColor(Color.RED);
+
+        // Points du cœur (forme approximative dans une fenêtre 100x100)
+        int[] xPoints = {50, 30, 10, 30, 50, 70, 90, 70};
+        int[] yPoints = {30, 10, 30, 60, 90, 60, 30, 10};
+
+        // Dessin du polygone du cœur
+        g.fillPolygon(xPoints, yPoints, xPoints.length);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
