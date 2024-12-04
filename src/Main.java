12c36078cb9427d758ad46040d48749a638d4be8
Etienne Gonella <etienne.gonella@gmail.com>
2024-12-04 09:56:25 +0100

    Message pour conflit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         //coucou camcam
-        System.out.println("Ceci est un test");
+        System.out.println("Salut Camille je suis heureux de travailler avec toi");
         }
     }
