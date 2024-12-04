911cae2f7f3537656c78787013b3de5df59afe61
Camille <camilleduval911@gmail.com>
2024-12-04 09:56:19 +0100

    git init

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         //coucou camcam
-        System.out.println("Ceci est un test");
+        System.out.println("Normalement il y aura conflit");
         }
     }
